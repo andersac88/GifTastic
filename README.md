6af827ab0d7909b5873f5a1b407b0b7f9d7bbd24
Andrew Andersen <andersac88@gmail.com>
2018-10-11 00:35:22 -0500

    a lot

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # GifTastic
+
+This is a program to run gifs from the Giphy API
+
+
+This project provides a great introduction to timers and ties it in with jquery, vanilla javascript, html and css.
+
+This project is currently maintained and updated by Andrew Andersen (andersac88@gmail.com) if you have any questions, feel free to contact me directly.
+
--- app.js
+++ app.js
@@ -1,68 +1,112 @@
 var topics = ["Bart Simpson", "Homer Simpson", "Ned Flanders", "Marge Simspon", "Maggie Simpson", "Lionel Hutz", "Ralph Wiggum", "Principal Skinner", "Mayor Quimby", "Troy McClure"];
-var i = 0;
-var moving = false;
+var more;
 
 function displayGIF() {
-  
+    console.log(this)
+    var more = $(this).attr("data-number")
     var gifName = $(this).attr("data-value");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifName + "&api_key=P0nquDLJ1q86xUkaky5VJsXRacogZQyw&limit=15";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifName + "&api_key=P0nquDLJ1q86xUkaky5VJsXRacogZQyw&limit=30";
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-        console.log(response);
-        console.log(i);
-        if (i === 0) {
+          console.log(response)
+            $(".crate").remove();
             $("img").remove();
-            for (i = 0; i < 5; i++){
+            $("p").remove()
+            for (var i = 0; i < 10; i++){  
+                var box = $("<div>");
+                box.addClass("crate")
+                $(".boxes").append(box);
                 var img = $("<img>");
                 img.attr("src", response.data[i].images.original_still.url);
-                img.attr("data-number", i)
+                img.attr("data-still", response.data[i].images.original_still.url);
+                img.attr("data-animate", response.data[i].images.original.url);
+                img.attr("data-state", "still")
                 img.addClass("m-3 gif")
-                $(".images").append(img);}
-                console.log(i);
-            } else if (i === 5)  {
-                $("img").remove();
-                for (i = 0; i < 15; i++) {
-                    var img = $("<img>");
-                    img.attr("src", response.data[i].images.original_still.url);
-                    img.attr("data-number", i)
-                    img.addClass("m-3 gif")
-                     $(".images").append(img);}
-                     i = 0;
-                    }    
-
-            $(".gif").on("click", function() {
-                console.log(this)
-                if (moving === false) {
-                var gifString = $(this).attr("data-number")
-                var gifNumber = parseInt(gifString);
-                console.log(gifNumber);
-                moving = true;          
-               $(this).attr("src", response.data[gifNumber].images.original.url)
-                } else if (moving === true) {
-                    var gifString = $(this).attr("data-number")
-                    var gifNumber = parseInt(gifString);
-                    console.log(gifNumber);               
-                   $(this).attr("src", response.data[gifNumber].images.original_still.url)
-                   moving = false;
-                }
-            });
-      });
+                $(box).append(img);
+                var title = $("<p>")
+                title.addClass("text-center text-capitalize text-truncate")
+                title.text(response.data[i].title)
+                $(box).append(title)
+                var rating = $("<p>")
+                rating.addClass("text-center text-uppercase")
+                rating.text("Rating " + response.data[i].rating)
+                $(box).append(rating)
+                var download = $("<a>")
+                download.attr("href", response.data[i].images.original.url)
+                download.attr("download", true);
+                download.addClass("text-center")
+                download.text("Click to Download")
+                $(box).append(download)
+            } moveGif() 
+               });
 }
 
 
+function moveGif() {
+    $(".gif").on("click", function() {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");}
+});
+}
+
+function moreGifs() {
+    console.log(this)
+    var more = $(this).attr("data-number")
+    var gifName = $(this).attr("data-value");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifName + "&api_key=P0nquDLJ1q86xUkaky5VJsXRacogZQyw&limit=30";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+            $(".crate").remove();
+            $("img").remove();
+            $("p").remove()
+            for (var i = 0; i < 20; i++){  
+                var box = $("<div>");
+                box.addClass("crate")
+                $(".boxes").append(box);
+                var img = $("<img>");
+                img.attr("src", response.data[i].images.original_still.url);
+                img.attr("data-still", response.data[i].images.original_still.url);
+                img.attr("data-animate", response.data[i].images.original.url);
+                img.attr("data-state", "still")
+                img.addClass("m-3 gif")
+                $(box).append(img);
+                var title = $("<p>")
+                title.addClass("text-center text-capitalize text-truncate")
+                title.text(response.data[i].title)
+                $(box).append(title)
+                var rating = $("<p>")
+                rating.addClass("text-center text-uppercase")
+                rating.text("Rating " + response.data[i].rating)
+                $(box).append(rating)
+                var download = $("<a>")
+                download.attr("href", response.data[i].images.original.url)
+                download.attr("download", true);
+                download.text("Click to Download")
+                $(box).append(download)
+            } moveGif() 
+               });
+}
 
 
 function searchButtons() {
     // Deleting the topics prior to adding new movies
     $(".header").empty();
     // Looping through the array of topics
-    for (var i = 0; i < topics.length; i++) {
+    for (var j = 0; j < topics.length; j++) {
       var a = $("<button>");
       a.addClass("topic-btn btn btn-warning mx-1");
-      a.attr("data-value", topics[i]);
-      a.text(topics[i]);
+      a.attr("data-value", topics[j]);
+      a.attr("data-number", 1)
+      a.text(topics[j]);
       $(".header").append(a);
     }
   }
@@ -77,7 +121,8 @@ function searchButtons() {
     });
 
 
-  $(document).on("click", ".topic-btn", displayGIF);
+  $(document).on("click", ".topic-btn", displayGIF); 
+  $(document).on("dblclick", ".topic-btn", moreGifs);
   searchButtons();
   
  
